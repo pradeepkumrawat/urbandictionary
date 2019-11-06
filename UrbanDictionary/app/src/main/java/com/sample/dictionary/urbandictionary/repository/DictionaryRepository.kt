bad6d1ff214bc4b0ae4e8f8837e9b17bea678ddf
pradeepkumrawat <pradeep.kumrawat@gmail.com>
2019-11-05 23:46:26 -0500

    modified DictionaryRepository to maintain result in cache

--- UrbanDictionary/app/src/main/java/com/sample/dictionary/urbandictionary/repository/DictionaryRepository.kt
+++ UrbanDictionary/app/src/main/java/com/sample/dictionary/urbandictionary/repository/DictionaryRepository.kt
@@ -3,36 +3,60 @@ package com.sample.dictionary.urbandictionary.repository
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.util.Log
 import com.sample.dictionary.urbandictionary.DictionaryApplication
 import okhttp3.Cache
+import okhttp3.CacheControl
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.time.Duration
+import java.util.concurrent.TimeUnit
 
 //DictionaryRepository create the retrofit instance by setting all the necessary details
 class DictionaryRepository {
     companion object {
-        private val BASE_URL : String = "https://mashape-community-urban-dictionary.p.rapidapi.com"
+        private val BASE_URL = "https://mashape-community-urban-dictionary.p.rapidapi.com"
         private val CAHCE_SIZE_IN_BYTES : Long = 1024*1024*5 //5MB cache
+        private val MAXSTALE = 60 * 60 * 24 * 7 // Offline cache available for 7 days
+        private val MAXAGE = 60 // read from cache for 60 seconds even if there is internet connection
+        private val HEADER_CACHE_CONTROL = "Cache-Control"
+        private val HEADER_PRAGMA = "Pragma"
+        private val CACH_NAME = "dictionarycache"
         private val TIMEOUT : Duration = Duration.ofSeconds(10) //10 Sec
         fun getRetrofitInstance() : Retrofit{
             //Creating HttpLoggingInterceptor instance
             val httpLoggingInterceptor : HttpLoggingInterceptor = HttpLoggingInterceptor()
             httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
+            val cache = Cache(File(DictionaryApplication.Companion.appContext?.cacheDir,CACH_NAME) ,CAHCE_SIZE_IN_BYTES)
             //Creating OkHttpClient instance
             var okHttpClient : OkHttpClient = OkHttpClient().newBuilder()
-                .cache(DictionaryApplication.Companion.appContext?.cacheDir?.let { Cache(it, CAHCE_SIZE_IN_BYTES) })
                 //Code to maintain the search results in cache
+                .cache(cache)
+                //Interceptor the reuqest and read from cache when device is offline
                 .addInterceptor { chain ->
                     var request = chain.request()
-                    request = if (hasNetwork(DictionaryApplication.appContext)!!)
-                        request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
-                    else
-                        request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+                    if (!hasNetwork(DictionaryApplication.appContext)!!) {
+
+                        request = request.newBuilder()
+                            .header(HEADER_CACHE_CONTROL, "public, only-if-cached, max-stale=$MAXSTALE")
+                            .removeHeader(HEADER_PRAGMA)
+                            .build()
+
+                    }
                     chain.proceed(request) }
+                //Network Interceptor the request data from network or read form cache
+                .addNetworkInterceptor { chain ->
+                    val response = chain.proceed(chain.request())
+                    response.newBuilder()
+                        .header(HEADER_CACHE_CONTROL, "public, max-age=$MAXAGE")
+                        .removeHeader(HEADER_PRAGMA)
+                        .build()}
+                //HTTP logging interceptor
                 .addInterceptor(httpLoggingInterceptor)
                 .connectTimeout(TIMEOUT)
                 .build()
